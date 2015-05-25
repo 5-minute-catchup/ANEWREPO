0f99f460b68bcbb323eb34bc20ba7d41b3736e27
Phoebe Hugh <phoebehugh@gmail.com>
2015-05-25 14:01:37 +0100

    changes facebook callback for heroku

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ var sessionData = session({
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    callbackURL: "https://fivemincatchup.herokuapp.com/"
   },
   function(accessToken, refreshToken, profile, done) {
 
