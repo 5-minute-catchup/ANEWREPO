b263b9c43373b3f05cf0db9ca4dbd6ad43ebaaff
Phoebe Hugh <phoebehugh@gmail.com>
2015-05-25 14:15:59 +0100

    Changes callback again

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ var sessionData = session({
 passport.use(new FacebookStrategy({
     clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/callback"
+    callbackURL: "/auth/facebook/callback"
   },
   function(accessToken, refreshToken, profile, done) {
 
